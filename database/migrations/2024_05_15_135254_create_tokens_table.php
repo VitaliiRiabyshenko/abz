014676a524deefdc44eef9ac8de8e622f4ebb645
Vitalii Riabyshenko <wibston1234@gmail.com>
2025-01-15 20:30:50 +0200

    add tables, models and user\position factory

---
+++ database/migrations/2024_05_15_135254_create_tokens_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tokens', function (Blueprint $table) {
+            $table->id();
+            $table->text('session', 64)->nullable();
+            $table->boolean('used')->default(0);
+            $table->string('token', 64)->unique();
+            $table->timestamp('expired_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tokens');
+    }
+}
