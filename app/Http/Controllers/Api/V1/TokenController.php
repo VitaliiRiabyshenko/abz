04d243d0aa00b44e1a1ef5ed2b589ee694999e03
Vitalii Riabyshenko <wibston1234@gmail.com>
2025-01-16 17:27:27 +0200

    add token route, middleware

---
+++ app/Http/Controllers/Api/V1/TokenController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Services\TokenService;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TokenResource;
+
+class TokenController extends Controller
+{
+    private $tokenService;
+
+    public function __construct(TokenService $tokenService)
+    {
+        $this->tokenService = $tokenService;
+    }
+
+    public function __invoke()
+    {
+        $token = $this->tokenService->getToken();
+
+        return new TokenResource($token);
+    }
+}
